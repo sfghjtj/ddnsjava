7de5f1a083cc22d0fac0fcf3b0d90174063ab0fd
houwei.zhao <houwei.zhao@ttpai.cn>
2023-04-05 11:03:13 +0800

    test

---
+++ src/main/java/pers/zhw/model/User.java
@@ -0,0 +1,17 @@
+package pers.zhw.model;
+
+import lombok.Data;
+
+/**
+ * @Author houwei.zhao@ttpai.cn on 2023/4/5.
+ */
+@Data
+public class User {
+
+    private int age;
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
