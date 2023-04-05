a4b8bc0f33646bd5adb9395773bff6582ec9bba1
houwei.zhao <houwei.zhao@ttpai.cn>
2023-04-05 15:16:33 +0800

    日志jar包完善

--- src/main/java/pers/zhw/model/User.java
+++ src/main/java/pers/zhw/model/User.java
@@ -1,17 +1,15 @@
 package pers.zhw.model;
 
+
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Author houwei.zhao@ttpai.cn on 2023/4/5.
  */
 @Data
+@Slf4j
 public class User {
 
     private int age;
-
-
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
 }
