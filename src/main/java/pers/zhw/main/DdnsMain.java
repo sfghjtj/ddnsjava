62cca30c9f553af08ba60ff6a935003807315c05
houwei.zhao <houwei.zhao@ttpai.cn>
2023-04-20 17:17:14 +0800

    删除多余代码

--- src/main/java/pers/zhw/main/DdnsMain.java
+++ src/main/java/pers/zhw/main/DdnsMain.java
@@ -1,10 +1,6 @@
 package pers.zhw.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
-
 import lombok.extern.slf4j.Slf4j;
 import pers.zhw.config.SpringConfig;
 
@@ -16,8 +12,5 @@ public class DdnsMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
         Runtime.getRuntime().addShutdownHook(new Thread(applicationContext::close));
-        do {
-            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(60 * 60L));
-        } while (true);
     }
 }
